4c7411f8b886ee98d32c540294d6bf0471307e4e
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-16 21:07:41 +0100

    add categorie migration model

---
+++ app/Models/Categories/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Categories;
+
+use App\Models\Product\Product;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    public $table = "categories";
+    protected $fillable = ['name'];
+    public $timestamps = true;
+    function Products()  {
+        return $this->hasMany(Product::class, 'cat_id', 'id');
+
+
+    }
+}
