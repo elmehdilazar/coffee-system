9cbf26c98974c0f2504b9920e33b384f4b22e0c0
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-16 20:05:53 +0100

    single product vieww home and addd producte single

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -296,10 +296,10 @@
                     <div class="menu-entry">
                         <a href="#" class="img" style="background-image: url({{asset("assets/images/".$product->image)}});"></a>
                         <div class="text text-center pt-4">
-                            <h3><a href="#"> {{ $product->name}}</a></h3>
+                            <h3><a href="{{ route('product.single', ['id'=>$product->id]) }}"> {{ $product->name}}</a></h3>
                             <p>{{ $product->description}}</p>
                             <p class="price"><span>${{ $product->price}}</span></p>
-                            <p><a href="#" class="btn btn-primary btn-outline-primary">showe</a></p>
+                            <p><a href="{{ route('product.single', ['id'=>$product->id]) }}" class="btn btn-primary btn-outline-primary">show</a></p>
                         </div>
                     </div>
                 </div>
