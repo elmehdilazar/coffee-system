9f0560e3e236133f890af0983f6ea4b48698cde7
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-18 23:56:34 +0100

    cart

--- resources/views/products/cart.blade.php
+++ resources/views/products/cart.blade.php
@@ -1,205 +1,205 @@
 @extends('layouts.app')
 
-@section("style")
-<style>
-    .ftco-cart button i {
-        color: #c49b63;
-    }
-
-    .ftco-cart .quantity-left-minus {
-        background: transparent;
-        padding: 16px 20px;
-    }
-
-    .ftco-cart .quantity-right-plus {
-        background: transparent;
-        padding: 16px 20px;
-    }
-
-    .ftco-cart button,
-    .ftco-cart .form-control {
-        height: 54px !important;
-        text-align: center;
-        border: 1px solid rgba(255, 255, 255, 0.1) !important;
-        color: #c49b63 !important;
-        padding: 0;
-    }
-
-    .ftco-cart .form-group {
-        position: relative;
-    }
-
-    .ftco-cart .form-group .form-control {
-        padding-right: 40px;
-    }
-
-    .ftco-cart .form-group .icon {
-        position: absolute;
-        top: 50%;
-        right: 20px;
-        font-size: 14px;
-        -webkit-transform: translateY(-50%);
-        -ms-transform: translateY(-50%);
-        transform: translateY(-50%);
-        color: #c49b63;
-    }
-
-    .ftco-cart .form-group .icon span {
-        color: #c49b63;
-    }
-
-    @media (max-width: 767.98px) {
-        .ftco-cart .form-group .icon {
-            right: 10px;
+@section('style')
+    <style>
+        .ftco-cart button i {
+            color: #c49b63;
         }
-    }
 
-    .ftco-cart .form-group .select-wrap {
-        position: relative;
-    }
+        .ftco-cart .quantity-left-minus {
+            background: transparent;
+            padding: 16px 20px;
+        }
 
-    .ftco-cart .form-group .select-wrap select {
-        font-size: 14px;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none;
-    }
+        .ftco-cart .quantity-right-plus {
+            background: transparent;
+            padding: 16px 20px;
+        }
 
-    .cart-list {
-        overflow-x: hidden;
-    }
+        .ftco-cart button,
+        .ftco-cart .form-control {
+            height: 54px !important;
+            text-align: center;
+            border: 1px solid rgba(255, 255, 255, 0.1) !important;
+            color: #c49b63 !important;
+            padding: 0;
+        }
 
-    .table-cart {
-        min-width: 1000px !important;
-        width: 100%;
-        text-align: center;
-    }
+        .ftco-cart .form-group {
+            position: relative;
+        }
 
-    .table-cart th {
-        font-weight: 500;
-    }
+        .ftco-cart .form-group .form-control {
+            padding-right: 40px;
+        }
 
-    .table-cart .thead-primary {
-        background: #c49b63;
-    }
+        .ftco-cart .form-group .icon {
+            position: absolute;
+            top: 50%;
+            right: 20px;
+            font-size: 14px;
+            -webkit-transform: translateY(-50%);
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+            color: #c49b63;
+        }
 
-    .table-cart .thead-primary tr th {
-        padding: 20px 10px;
-        color: #fff !important;
-        border: 1px solid transparent !important;
-    }
+        .ftco-cart .form-group .icon span {
+            color: #c49b63;
+        }
 
-    .table-cart tbody tr td {
-        text-align: center !important;
-        vertical-align: middle;
-        padding: 40px 10px;
-        border: 1px solid transparent !important;
-        border-bottom: 1px solid rgba(255, 255, 255, 0.1) !important;
-    }
+        @media (max-width: 767.98px) {
+            .ftco-cart .form-group .icon {
+                right: 10px;
+            }
+        }
 
-    .table-cart tbody tr td.product-remove a {
-        border: 1px solid rgba(255, 255, 255, 0.1);
-        padding: 5px 10px;
+        .ftco-cart .form-group .select-wrap {
+            position: relative;
+        }
 
-    }
+        .ftco-cart .form-group .select-wrap select {
+            font-size: 14px;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+        }
 
-    .table-cart tbody tr td.product-remove a:hover {
-        background: #c49b63;
-    }
+        .cart-list {
+            overflow-x: hidden;
+        }
 
-    .table-cart tbody tr td.product-remove a:hover span {
-        color: #000;
-    }
+        .table-cart {
+            min-width: 1000px !important;
+            width: 100%;
+            text-align: center;
+        }
 
-    .table-cart tbody tr td.product-remove button {
-        border: 1px solid rgba(255, 255, 255, 0.1);
-        padding: 5px 10px;
-        background-color: transparent;
-    }
+        .table-cart th {
+            font-weight: 500;
+        }
 
-    .table-cart tbody tr td.product-remove button:hover {
-        background: #c49b63;
-    }
+        .table-cart .thead-primary {
+            background: #c49b63;
+        }
 
-    .table-cart tbody tr td.product-remove button:hover span {
-        color: #000000;
-    }
+        .table-cart .thead-primary tr th {
+            padding: 20px 10px;
+            color: #fff !important;
+            border: 1px solid transparent !important;
+        }
 
-    .table-cart tbody tr td.quantity {
-        width: 20%;
-    }
+        .table-cart tbody tr td {
+            text-align: center !important;
+            vertical-align: middle;
+            padding: 40px 10px;
+            border: 1px solid transparent !important;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.1) !important;
+        }
 
-    .table-cart tbody tr td.image-prod .img {
-        display: block;
-        width: 100px;
-        height: 100px;
-        margin: 0 auto;
-    }
+        .table-cart tbody tr td.product-remove a {
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            padding: 5px 10px;
 
-    .table-cart tbody tr td.product-name {
-        width: 30%;
+        }
 
-    }
+        .table-cart tbody tr td.product-remove a:hover {
+            background: #c49b63;
+        }
 
-    .table-cart tbody tr td.product-name h3 {
-        font-size: 16px;
-        text-transform: uppercase;
-        color: white;
-    }
-    .table-cart tbody tr td.product-name p {
-       color: rgba(255, 255, 255, 0.258);
-    }
+        .table-cart tbody tr td.product-remove a:hover span {
+            color: #000;
+        }
 
-    .table-cart tbody tr td.total,
-    .table-cart tbody tr td.price {
-        color: #fff;
-    }
+        .table-cart tbody tr td.product-remove button {
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            padding: 5px 10px;
+            background-color: transparent;
+        }
 
-    .cart-wrap .btn-primary {
-        display: block;
-        width: 100%;
-    }
+        .table-cart tbody tr td.product-remove button:hover {
+            background: #c49b63;
+        }
 
-    .cart-total {
-        width: 100%;
-        display: block;
-        border: 1px solid rgba(255, 255, 255, 0.1);
-        padding: 20px;
-    }
+        .table-cart tbody tr td.product-remove button:hover span {
+            color: #000000;
+        }
 
-    .cart-total h3 {
-        font-size: 20px;
-        text-transform: uppercase;
-    }
+        .table-cart tbody tr td.quantity {
+            width: 20%;
+        }
 
-    .cart-total p {
-        width: 100%;
-        display: block;
-    }
+        .table-cart tbody tr td.image-prod .img {
+            display: block;
+            width: 100px;
+            height: 100px;
+            margin: 0 auto;
+        }
 
-    .cart-total p span {
-        display: block;
-        width: 50%;
-    }
+        .table-cart tbody tr td.product-name {
+            width: 30%;
 
-    .cart-total p.total-price span {
-        text-transform: uppercase;
-    }
+        }
 
-    .cart-total p.total-price span:last-child {
-        color: #c49b63;
-    }
+        .table-cart tbody tr td.product-name h3 {
+            font-size: 16px;
+            text-transform: uppercase;
+            color: white;
+        }
 
-    .cart-total hr {
-        background: rgba(255, 255, 255, 0.1);
-    }
+        .table-cart tbody tr td.product-name p {
+            color: rgba(255, 255, 255, 0.258);
+        }
 
-</style>
+        .table-cart tbody tr td.total,
+        .table-cart tbody tr td.price {
+            color: #fff;
+        }
+
+        .cart-wrap .btn-primary {
+            display: block;
+            width: 100%;
+        }
+
+        .cart-total {
+            width: 100%;
+            display: block;
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            padding: 20px;
+        }
+
+        .cart-total h3 {
+            font-size: 20px;
+            text-transform: uppercase;
+        }
+
+        .cart-total p {
+            width: 100%;
+            display: block;
+        }
+
+        .cart-total p span {
+            display: block;
+            width: 50%;
+        }
+
+        .cart-total p.total-price span {
+            text-transform: uppercase;
+        }
+
+        .cart-total p.total-price span:last-child {
+            color: #c49b63;
+        }
+
+        .cart-total hr {
+            background: rgba(255, 255, 255, 0.1);
+        }
+    </style>
 @endsection
 @section('content')
-@php
-   $total=0;
-@endphp
+    @php
+        $total = 0;
+    @endphp
     <section class="home-slider owl-carousel">
 
         <div class="slider-item" style="background-image: url({{ asset('assets/images/bg_3.jpg)') }};"
@@ -237,7 +237,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @if ($carts->count()>0)
+                                    @if ($carts->count() > 0)
                                         @foreach ($carts as $cart)
                                             <tr class="text-center">
                                                 <td class="product-remove">
@@ -272,7 +272,7 @@
                                                     </div>
                                                 </td>
 
-                                                <td class="total">$ {{ $total+=$cart->Product->price * $cart->qte }} </td>
+                                                <td class="total">$ {{ $total += $cart->Product->price * $cart->qte }} </td>
                                             </tr><!-- END TR-->
                                         @endforeach
                                     @else
@@ -290,36 +290,42 @@
                             </table>
                         </div>
                     </div>
-                </div> @if ($carts->count()>0)
-                <div class="row justify-content-end">
-                    <div class="col col-lg-3 col-md-6 mt-5 cart-wrap ftco-animate">
-                        <div class="cart-total mb-3">
-                            <h3>Cart Totals</h3>
-                            <p class="d-flex">
-                                <span>Subtotal</span>
-                                <span>${{$total}}</span>
-                            </p>
-                            <p class="d-flex">
-                                <span>Delivery</span>
-                                <span>$0.00</span>
-                            </p>
-                            <p class="d-flex">
-                                <span>Discount</span>
-                                <span>$3.00</span>
-                            </p>
-                            <hr>
-                            <p class="d-flex total-price">
-                                <span>Total</span>
-                                <span>$17.60</span>
+                </div>
+                @if ($carts->count() > 0)
+                    <div class="row justify-content-end">
+                        <div class="col col-lg-3 col-md-6 mt-5 cart-wrap ftco-animate">
+                            <div class="cart-total mb-3">
+                                <h3>Cart Totals</h3>
+                                <p class="d-flex">
+                                    <span>Subtotal</span>
+                                    <span>${{ $total }}</span>
+                                </p>
+                                <p class="d-flex">
+                                    <span>Delivery</span>
+                                    <span>$0.00</span>
+                                </p>
+                                <p class="d-flex">
+                                    <span>Discount</span>
+                                    <span>$3.00</span>
+                                </p>
+                                <hr>
+                                <p class="d-flex total-price">
+                                    <span>Total</span>
+                                    <span>$17.60</span>
+                                </p>
+                            </div>
+
+                            <form id="check" method="POST" action="{{ route('cart.precheck') }}">
+                                @csrf
+                                <input type="hidden" name="price" value="{{$total}}">
+                            </form>
+                            <p class="text-center"><button type="submit" form="check" name="submit"
+                                    class="btn btn-primary py-3 px-4">Proceed to Checkout</button>
+
                             </p>
                         </div>
-
-
-                        <p class="text-center"><a href="checkout.html" class="btn btn-primary py-3 px-4">Proceed to Checkout</a>
-
-                        </p>
                     </div>
-                </div>@endif
+                @endif
             </div>
         </section>
     </div>
