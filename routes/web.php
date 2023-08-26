20695b74ee800dccdb28a42a80d87749952b4c79
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-26 23:21:09 +0100

    web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Models\Order\Reviews;
+use App\Models\Product\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Products\ProductsController;
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Products\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,30 +20,35 @@ use App\Http\Controllers\User\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $products = Product::select()->orderBy('id', 'DESC')->take("4")->get();
+    $reviews = Reviews::select()->orderBy('id', 'DESC')->take("6")->get();
+    return view('home', compact("products", "reviews"));
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+Route::get('/services', [App\Http\Controllers\HomeController::class, 'services'])->name('services');
+Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('about');
+Route::get('/contact', [App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
 
 Route::prefix('products')->group(function () {
     Route::get('singleProduct/{id}',[ProductsController::class,'singleProduct'])->name('product.single');
-    Route::post('singleProduct/{id}',[ProductsController::class,'addCart'])->name('cart.add');
+    Route::post('singleProduct/{id}',[ProductsController::class,'addCart'])->name('cart.add')->middleware("auth:web");
     Route::get('menu',[ProductsController::class,'menu'])->name('product.menu');
 });
 
 Route::prefix('cart')->group(function () {
 
-        Route::get('show', [ProductsController::class, 'showCart'])->name('cart.show');
-        Route::delete('delete/{id}', [ProductsController::class, 'deleteCart'])->name('cart.delete');
-        Route::post('precheck', [ProductsController::class, 'prepareCheckout'])->name('cart.precheck');
+        Route::get('show', [ProductsController::class, 'showCart'])->name('cart.show')->middleware("auth:web");
+        Route::delete('delete/{id}', [ProductsController::class, 'deleteCart'])->name('cart.delete')->middleware("auth:web");
+        Route::post('precheck', [ProductsController::class, 'prepareCheckout'])->name('cart.precheck')->middleware("auth:web");
         Route::get('checkout', [ProductsController::class, 'checkout'])->name('cart.checkout')->middleware("check.for.price");
         Route::post('checkout', [ProductsController::class, 'storeOrder'])->name('checkout.proccessing')->middleware("check.for.price");
         Route::get('paiment', [ProductsController::class, 'paypal_check'])->name('checkout.paiment')->middleware("check.for.price");
         Route::get('success_paiment', [ProductsController::class, 'success_paiment'])->name('checkout.success_paiment')->middleware("check.for.price");
 });
-Route::prefix('user')->group(function () {
+Route::prefix('user')->middleware("auth:web")->group(function () {
  Route::post('Add_booking',[UserController::class, 'booking'])->name("book");
  Route::get('Display_orders',[UserController::class, 'displaysOrders'])->name("user.orders");
  Route::get('Display_booking',[UserController::class, 'displaysBooking'])->name("user.booking");
