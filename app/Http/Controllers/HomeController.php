4073572a2f5cc5b403bea787fd1b5e31405da444
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-26 23:20:32 +0100

    homecontroler

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order\Reviews;
 use App\Models\Product\Product;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+     
     }
 
     /**
@@ -22,8 +23,20 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function home()
     {  $products=Product::select()->orderBy('id', 'DESC')->take("4")->get();
-        return view('home',compact("products"));
+        $reviews=Reviews::select()->orderBy('id', 'DESC')->take("6")->get();
+        return view('home',compact("products","reviews"));
+    }
+    function about()  {
+        $products = Product::select()->orderBy('id', 'DESC')->take("4")->get();
+        $reviews = Reviews::select()->orderBy('id', 'DESC')->take("6")->get();
+  return view("pages.about", compact("products", "reviews"));
+    }
+    function services()  {
+        return view("pages.services");
+    }
+    function contact()  {
+        return view("pages.contact");
     }
 }
