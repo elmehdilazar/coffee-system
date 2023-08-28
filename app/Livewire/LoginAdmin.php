539096074cd68b7ebc984badca5ae452f34ea35e
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-29 00:09:21 +0100

    login add

---
+++ app/Livewire/LoginAdmin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class LoginAdmin extends Component
+{
+    public  $email;
+    public $password;
+
+    protected $rules = [
+
+        'email' => 'required',
+        'password' => 'required'
+    ];
+
+
+
+    public function render()
+    {
+         return view('livewire.login-admin');
+    }
+    public function login()  {
+   $this->validate();
+
+        if (auth()->guard('admin')->attempt(['email' => $this->email, 'password' => $this->password])) {
+            session()->flash('success', 'user connected successfully .');
+            return  Redirect::route("admin.home");
+        } else {
+            session()->flash('error', 'user not  connected.');
+            return  redirect()->back();
+        }
+
+    }
+
+}
