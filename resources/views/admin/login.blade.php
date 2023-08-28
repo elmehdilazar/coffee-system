4018cba07107776a4026414ec5fdc167262b1555
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-29 00:10:37 +0100

    admin view login blade app nav login-admin livewire

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,10 @@
+@extends("layouts.admins.app")
+@section('auth')
+    @php
+        $chek = true;
+  
+    @endphp
+@endsection
+@section('content')
+   @livewire("login-admin")
+@endsection
---
+++ resources/views/layouts/admins/message.blade.php
@@ -0,0 +1,51 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+@if (session()->has("success"))
+
+
+<div class="position-fixed top-25 start-50  mt-5 translate-middle" style="z-index: 3000;">
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <span class="fas fa-bullhorn me-1"></span>
+{{ session()->get("success") }}
+  <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+
+
+
+</div>
+
+@endif
+
+
+@if (session()->has("error"))
+
+<div class="position-fixed top-25 start-50  mt-5 translate-middle" style="z-index: 3000;">
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <span class="fas fa-bullhorn me-1"></span>
+{{ session()->get("error") }}
+  <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+
+
+
+</div>>
+@endif
+
+@if (session()->has("warning"))
+
+
+<div class="position-fixed top-25 start-50  mt-5 translate-middle" style="z-index: 3000;">
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <span class="fas fa-bullhorn me-1"></span>
+{{ session()->get("warning") }}
+  <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+
+
+
+</div>
+@endif
+
