194fcac61b05c8cad6f85ebbbaac95c5f9c1be79
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-28 02:51:52 +0100

    intrgrate admin dash

---
+++ app/Models/Admin/admin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class admin extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+    public $timestamps = true;
+}
