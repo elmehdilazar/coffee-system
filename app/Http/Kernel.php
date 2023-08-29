9ac4bc85d4f094405c854512949f1450c20bc5ce
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-29 13:34:30 +0100

    add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'check.for.price' => \App\Http\Middleware\checkPrice::class,
+        'check.for.auth' => \App\Http\Middleware\checkAuthAdmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
