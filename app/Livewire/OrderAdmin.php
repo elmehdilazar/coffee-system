8cd63adcea702b5e91d8f494503c914e797fd71b
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-09-03 17:37:44 +0100

    admin order back

---
+++ app/Livewire/OrderAdmin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Order\Order;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class OrderAdmin extends Component
+{
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $status = '';
+    public $search = '';
+
+    public function render()
+    {
+        return view('livewire.order-admin')->with([
+            'orders' => Order::where('status', 'like', '%' . $this->search . '%')
+                ->orWhere('first_name', 'like', '%' . $this->search . '%')
+                ->orWhere('phone', 'like', '%' . $this->search . '%')
+                ->orWhere('email', 'like', '%' . $this->search . '%')
+                ->paginate(6)
+        ]);
+    }
+    function updateStatus($id)
+    {
+        $order = Order::find($id);
+        $order->status = $this->status;
+        $order->update();
+        $this->resetPage();
+    }
+}
