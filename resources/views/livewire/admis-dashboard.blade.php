9b465c2ca56e6c46c0b866b26411def5c6fc9763
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-09-06 01:56:47 +0100

    admin view  admin

--- resources/views/livewire/admis-dashboard.blade.php
+++ resources/views/livewire/admis-dashboard.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @include("layouts.admins.message")
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
         <div class="d-block mb-4 mb-md-0">
             <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
